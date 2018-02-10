9de25e931e6fe460f7bf80fdbe7bee43898cba9e
Ravi Chandra Kolli <ravichandra.vjas9@gmail.com>
2018-02-10 15:48:23 -0600

    defect fix

--- ArrayProblems/src/com/leetcode/algo/MatrixToeplitz.java
+++ ArrayProblems/src/com/leetcode/algo/MatrixToeplitz.java
@@ -10,12 +10,12 @@ public class MatrixToeplitz {
 		int rowLength = ar.length;
 		int[] firstRow = ar[0];
 		int columnLenth = firstRow.length; 
-		int index = 0;
+		int index = 1;
 		while(index < rowLength) {
 			
-			for(int i = index; i<columnLenth;i++) {
+			for(int i = 1; i<= columnLenth;i++) {
 				
-				if(i+1 < columnLenth && ar[1][i+1] != ar[0][i]) {
+				if(i+1 <= columnLenth && ar[index-1][i-1] != ar[index][i]) {
 					return false;
 				}
 			}
@@ -35,6 +35,10 @@ public class MatrixToeplitz {
 		int[][] matrix2 = {{1,2},{2,2}};
 		ismatrixToeplitz = matrixToeplitz(matrix2);
 		assertEquals(false, ismatrixToeplitz);
+		
+		int[][] matrix3 = {{1,2,3},{2,1,2},{3,2,1}};
+		ismatrixToeplitz = matrixToeplitz(matrix3);
+		assertEquals(true, ismatrixToeplitz);
 	}
 
 }
