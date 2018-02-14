4c1635499faca3a81447922704e21e65fcdeaf1a
Ravi Chandra Kolli <ravichandra.vjas9@gmail.com>
2018-02-13 19:35:25 -0600

    Dynamic Programmin leet code 62

---
+++ DynamicProgramming/src/com/algo/ravi/UniquePaths_62.java
@@ -0,0 +1,42 @@
+package com.algo.ravi;
+
+import org.junit.jupiter.api.Test;
+
+public class UniquePaths_62 {
+	
+	//m - rows
+	//n - columns
+	public int findUniquePaths(int m, int n) {
+		
+		int matrix[][] = new int[m][n];
+		int i = 0,j = 0;
+		
+		for(int z=0;z<m*n;z++) {
+			if(j == n) {
+				i++;
+				j = 0;
+			}
+			
+			if(i == 0 && j==0) {
+				matrix[i][j] = 1;
+				j++;
+			} else {
+				if(j > 0 && i >0 ) {
+					for(int r=1;r<j+1;r++) {
+						matrix[i][j] = matrix[i][j-1] + matrix[i-1][r];
+					}
+				}else {
+					matrix[i][j] = 1;
+				}
+				j++;
+			}
+		}
+		
+		return matrix[m-1][n-1];
+	}
+	
+	@Test
+	public void testFindUniquePaths() {
+		System.out.println(findUniquePaths(3, 3));
+	}
+}
